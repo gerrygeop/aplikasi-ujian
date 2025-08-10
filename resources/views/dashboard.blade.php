a9f9d2e802f0d900198427072bf6c1f04412ba65
gerrygeop <gerryja28@gmail.com>
2025-08-10 08:22:19 +0800

    feat: dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,34 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.main')
+@section('content')
+	<div class="flex flex-col px-5 mt-5">
+		<div class="w-full flex justify-between items-center">
+			<div class="flex flex-col gap-1">
+				<p class="font-extrabold text-[30px] leading-[45px]">Dashboard</p>
+				<p class="text-[#7F8190]">Provide high quality for best students</p>
+			</div>
+		</div>
+	</div>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+	<div class="course-list-container flex flex-col px-5 mt-[30px] gap-[30px]">
+		<div class="course-list-header flex flex-nowrap justify-between pb-4 pr-10 border-b border-[#EEEEEE]">
+			<div class="flex shrink-0 w-[300px]">
+				<p class="text-[#7F8190]">Course</p>
+			</div>
+			<div class="flex justify-center shrink-0 w-[150px]">
+				<p class="text-[#7F8190]">Date Created</p>
+			</div>
+			<div class="flex justify-center shrink-0 w-[170px]">
+				<p class="text-[#7F8190]">Category</p>
+			</div>
+			<div class="flex justify-center shrink-0 w-[120px]">
+				<p class="text-[#7F8190]">Action</p>
+			</div>
+		</div>
+
+		<div class="list-items flex flex-nowrap justify-between pr-10">
+			<div class="flex shrink-0 w-full items-center justify-center">
+				<p class="italic">Empty</p>
+			</div>
+		</div>
+	</div>
+@endsection
